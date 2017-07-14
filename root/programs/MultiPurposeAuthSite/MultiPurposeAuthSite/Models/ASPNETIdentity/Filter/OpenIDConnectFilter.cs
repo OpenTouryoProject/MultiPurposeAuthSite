b6801ac98f6c8b2f3d6a27106afeab32a8b090d1
daisukenishino <daisuke.nishino.zg@hitachi-solutions.com>
2017-07-14 17:18:52 +0900

    Now, I am fixing #41.  Prepared a frame.  It complete if implement the contents.

---
+++ root/programs/MultiPurposeAuthSite/MultiPurposeAuthSite/Models/ASPNETIdentity/Filter/OpenIDConnectFilter.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Web;
+
+namespace MultiPurposeAuthSite.Models.ASPNETIdentity.Filter
+{
+    public class OpenIDConnectFilter : Stream
+    {
+        #region Member
+        /// <summary>HttpResponse</summary>
+        private HttpResponse _response;
+        /// <summary>Stream</summary>
+        private Stream _responseStream;
+        /// <summary>MemoryStream</summary>
+        private MemoryStream _tempBuffer;
+        #endregion
+
+        #region Property
+        /// <summary>HttpResponse</summary>
+        private HttpResponse Response
+        {
+            get { return this._response; }
+        }
+        /// <summary>Stream</summary>
+        private Stream ResponseStream
+        {
+            get { return this._responseStream; }
+        }
+        /// <summary>MemoryStream</summary>
+        private MemoryStream TempBuffer
+        {
+            get { return this._tempBuffer; }
+        }
+        #endregion
+
+        #region Constructor
+
+        /// <summary>Constructor</summary>
+        /// <param name="context">HttpContext</param>
+        public OpenIDConnectFilter(HttpContext context)
+        {
+            // レスポンスオブジェクト参照を取得
+            this._response = context.Response;
+            // レスポンスフィルタチェインを構築
+            this._responseStream = context.Response.Filter;
+            // バッファを構築
+            this._tempBuffer = new MemoryStream();
+        }
+
+        #endregion
+
+        #region Write & Flush
+
+        /// <summary>CanWrite</summary>
+        public override bool CanWrite
+        {
+            get { return true; }
+        }
+
+        /// <summary>Write</summary>
+        /// <param name="buffer">byte[]</param>
+        /// <param name="offset">offset</param>
+        /// <param name="count">count</param>
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            if (Response.StatusCode == 200 && Response.ContentType.ToLower().Contains("application/json"))
+            {
+                // レスポンスをバッファに蓄える
+                TempBuffer.Write(buffer, offset, count);
+            }
+            else
+            {
+                ResponseStream.Write(buffer, offset, count);
+            }
+        }
+
+        /// <summary>Flush</summary>
+        public override void Flush()
+        {
+            byte[] bb = TempBuffer.GetBuffer();
+
+            if (bb != null && bb.Length > 0)
+            {
+                // 書き換え処理
+                Encoding enc = Response.ContentEncoding;
+                string content = enc.GetString(bb);
+
+                content = content.Replace("書き換え前", "書き換え後");
+
+                // ・正規表現でaccess_tokenを抜き出す。
+                // ・access_tokenがJWTである場合、payloadからscopeを削除する。
+                // ・編集したpayloadを再度JWTとして署名する。
+                // ・このJWTをresponseにid_tokenを追加する。
+
+                bb = enc.GetBytes(content);
+            }
+
+            ResponseStream.Write(bb, 0, bb.Length);
+            ResponseStream.Flush();
+        }
+
+        #endregion
+
+        #region その他
+
+        /// <summary>CanRead</summary>
+        public override bool CanRead
+        {
+            get { return false; }
+        }
+
+        /// <summary>Read</summary>
+        /// <param name="buffer">byte[]</param>
+        /// <param name="offset">int</param>
+        /// <param name="count">int</param>
+        /// <returns></returns>
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            throw new NotSupportedException();
+        }
+
+        /// <summary>CanSeek</summary>
+        public override bool CanSeek
+        {
+            get { return false; }
+        }
+
+        /// <summary>Seek</summary>
+        /// <param name="offset">long</param>
+        /// <param name="origin">SeekOrigin</param>
+        /// <returns>long</returns>
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            throw new NotSupportedException();
+        }
+
+        /// <summary>Length</summary>
+        public override long Length
+        {
+            get { throw new NotSupportedException(); }
+        }
+
+        /// <summary>Position</summary>
+        public override long Position
+        {
+            get { throw new NotSupportedException(); }
+            set { throw new NotSupportedException(); }
+        }
+
+        /// <summary>SetLength</summary>
+        /// <param name="value">long</param>
+        public override void SetLength(long value)
+        {
+            throw new NotSupportedException();
+        }
+
+        #endregion
+    }
+}
---
+++ root/programs/MultiPurposeAuthSite/MultiPurposeAuthSite/Models/ASPNETIdentity/Filter/OpenIDConnectModule.cs
@@ -0,0 +1,103 @@
+﻿//**********************************************************************************
+//* Copyright (C) 2007,2016 Hitachi Solutions,Ltd.
+//**********************************************************************************
+
+#region Apache License
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License. 
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+#endregion
+
+//**********************************************************************************
+//* クラス名        ：OpenIDConnectModule
+//* クラス日本語名  ：OpenIDConnectModule（ライブラリ）
+//*
+//* 作成日時        ：－
+//* 作成者          ：－
+//* 更新履歴        ：－
+//*
+//*  日時        更新者            内容
+//*  ----------  ----------------  -------------------------------------------------
+//*  2017/07/14  西野 大介         新規
+//**********************************************************************************
+
+using System;
+using System.Web;
+using MultiPurposeAuthSite.Models.ASPNETIdentity;
+
+namespace MultiPurposeAuthSite.Models.ASPNETIdentity.Filter
+{
+    /// <summary>
+    /// モジュールを使用するには、Web の Web.config ファイルでこの
+    /// モジュールを設定し、IIS に登録する必要があります。詳細については、
+    /// 次のリンクを参照してください: http://go.microsoft.com/?linkid=8101007
+    /// </summary>
+    public class OpenIDConnectModule : IHttpModule
+    {
+
+        #region IHttpModule Members
+
+        /// <summary>Constructor</summary>
+        public OpenIDConnectModule() { }
+
+        /// <summary>Dispose</summary>
+        public void Dispose()
+        {
+            //後処理用コードはここに追加します。
+        }
+
+        /// <summary>Init</summary>
+        /// <param name="context">HttpApplication</param>
+        public void Init(HttpApplication context)
+        {
+            //context.LogRequest += new EventHandler(OnLogRequest);
+            context.BeginRequest += new EventHandler(OnBeginRequest);
+            //context.EndRequest += new EventHandler(OnEndRequest);
+        }
+
+        #endregion
+
+        /// <summary>OnLogRequest</summary>
+        /// <param name="source"></param>
+        /// <param name="e"></param>
+        private void OnLogRequest(Object source, EventArgs e)
+        {
+            // LogRequestのロジックはここに挿入
+        }
+
+        /// <summary>OnBeginRequest</summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnBeginRequest(Object sender, EventArgs e)
+        {
+            // EndRequestのロジックはここに挿入
+
+            HttpApplication application = (HttpApplication)sender;
+            HttpContext context = application.Context;
+            if (context.Request.Url.AbsolutePath.IndexOf(ASPNETIdentityConfig.OAuthBearerTokenEndpoint) != -1)
+            {
+                //レスポンス内容を参照して書き換え
+                HttpResponse response = context.Response;
+                response.Filter = new OpenIDConnectFilter(context);
+            }
+        }
+
+        /// <summary>OnEndRequest</summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnEndRequest(object sender, EventArgs e)
+        {
+            // EndRequestのロジックはここに挿入
+        }
+    }
+}
