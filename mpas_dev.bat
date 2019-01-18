8db886799d4bba41a62579bb9e293f03531ce244
daisukenishino2 <nishi_74322014@ksj.biglobe.ne.jp>
2019-01-18 18:43:08 +0900

    https://github.com/OpenTouryoProject/OpenTouryo/commit/becb456671b0935eb57aa4a44647b9d03b8f80d0 https://github.com/OpenTouryoProject/OpenTouryo/commit/0fadc33aafe8503f6a42136263afea7e9976ce65

--- mpas_dev.bat
+++ mpas_dev.bat
@@ -4,3 +4,4 @@
 @echo 本ファイルを一つ上のフォルダ階層に配置してダブルクリックして実行する。
 xcopy /E /Y /I "OpenTouryo\root\programs\CS\Frameworks\Infrastructure\Build_net47" "MultiPurposeAuthSite\root\programs\OpenTouryoAssemblies\Build_net47\"
 xcopy /E /Y /I "OpenTouryo\root\programs\CS\Frameworks\Infrastructure\Build_netcore20" "MultiPurposeAuthSite\root\programs\OpenTouryoAssemblies\Build_netcore20\"
+xcopy /E /Y /I "OpenTouryo\root\programs\CS\Frameworks\Infrastructure\Build_netcore30" "MultiPurposeAuthSite\root\programs\OpenTouryoAssemblies\Build_netcore30\"
