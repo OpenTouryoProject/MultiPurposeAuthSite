0649f508c4d42aa76365012bd76c3c3dfc38adb4
daisukenishino2 <nishi_74322014@ksj.biglobe.ne.jp>
2022-04-05 10:17:52 +0900

    Tweak.

--- mpas_dev.bat
+++ mpas_dev.bat
@@ -9,4 +9,4 @@
 @rem 当該フォルダに本バッチファイルをコピーして実行することを想定している。
 
 xcopy /E /Y /I "OpenTouryo\root\programs\CS\Frameworks\Infrastructure\Build_net48" "MultiPurposeAuthSite\root\programs\OpenTouryoAssemblies\Build_net48\"
-xcopy /E /Y /I "OpenTouryo\root\programs\CS\Frameworks\Infrastructure\Build_netcore50" "MultiPurposeAuthSite\root\programs\OpenTouryoAssemblies\Build_netcore50\"
+xcopy /E /Y /I "OpenTouryo\root\programs\CS\Frameworks\Infrastructure\Build_netcore60" "MultiPurposeAuthSite\root\programs\OpenTouryoAssemblies\Build_netcore60\"
