405eb1275280934acaf29b53d3d49b4d08a3b1b0
nishi_74322014 <nishi_74322014@ksj.biglobe.ne.jp>
2026-03-30 11:02:13 +0900

    Reboot:3

--- mpas_dev.bat
+++ mpas_dev.bat
@@ -9,4 +9,6 @@
 @rem 当該フォルダに本バッチファイルをコピーして実行することを想定している。
 
 xcopy /E /Y /I "OpenTouryo\root\programs\CS\Frameworks\Infrastructure\Build_net48" "MultiPurposeAuthSite\root\programs\OpenTouryoAssemblies\Build_net48\"
-xcopy /E /Y /I "OpenTouryo\root\programs\CS\Frameworks\Infrastructure\Build_netcore60" "MultiPurposeAuthSite\root\programs\OpenTouryoAssemblies\Build_netcore60\"
+xcopy /E /Y /I "OpenTouryo\root\programs\CS\Frameworks\Infrastructure\Build_netcore100" "MultiPurposeAuthSite\root\programs\OpenTouryoAssemblies\Build_netcore100\"
+
+pause
